9a070c546aff85a7993cc1205a869bd783ba1d39
fazair1 <fauzan.irfanto1@gmail.com>
2025-04-10 20:42:19 +0700

    Initial commit

---
+++ src/main/java/com/juaracoding/kepul/KepulApplication.java
@@ -0,0 +1,13 @@
+package com.juaracoding.kepul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KepulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KepulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juaracoding/kepul/KepulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juaracoding.kepul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KepulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
