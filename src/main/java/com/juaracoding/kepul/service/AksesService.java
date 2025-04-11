7beca332c74ff45151e446818487fef5854b5e0a
fazair1 <fauzan.irfanto1@gmail.com>
2025-04-11 17:02:00 +0700

    feat: akses sevice/controller

---
+++ src/main/java/com/juaracoding/kepul/service/AksesService.java
@@ -0,0 +1,56 @@
+package com.juaracoding.kepul.service;
+
+import com.juaracoding.kepul.core.IService;
+import com.juaracoding.kepul.model.Akses;
+import com.juaracoding.kepul.model.Menu;
+import com.juaracoding.kepul.repositories.AksesRepo;
+import com.juaracoding.kepul.repositories.MenuRepo;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class AksesService implements IService<Akses> {
+
+    @Autowired
+    private AksesRepo aksesRepo;
+
+    @Override
+    public ResponseEntity<Object> delete(Long id, HttpServletRequest request) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Object> save(Akses akses, HttpServletRequest request) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Object> update(Long id, Akses akses, HttpServletRequest request) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Object> findAll(Pageable pageable, HttpServletRequest request) {
+        List<Akses> list = aksesRepo.findAll();
+
+        return ResponseEntity.status(HttpStatus.OK).body(list);
+    }
+
+    @Override
+    public ResponseEntity<Object> findById(Long id, HttpServletRequest request) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Object> findByParam(Pageable pageable, String columnName, String value, HttpServletRequest request) {
+        return null;
+    }
+}
